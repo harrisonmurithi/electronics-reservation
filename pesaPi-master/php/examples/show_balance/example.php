d8d3c125faab4c328e543f13cee0faa729cf724b
harrisonmurithi <harrmury@gmail.com>
2016-10-17 20:17:00 +0300

    removed some files

--- pesaPi-master/php/examples/show_balance/example.php
+++
@@ -1,9 +0,0 @@
-<?php
-set_include_path("../../local_include:../../include:" . get_include_path());
-require_once("PLUSPEOPLE/autoload.php");
-
-// Simple syncronisation example
-$pesa = new PLUSPEOPLE\PesaPi\PesaPi();
-print "Available balance: " . $pesa->availableBalance() . "\n";
-
-?>
